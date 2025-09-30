53f559e91a322a75782dd46bbd637510271096af
Dan Bowles <daniel.bowles@gmail.com>
2025-09-30 15:27:56 -0400

    :monocle_face: Adding basic actions, some phoenix integration

---
+++ lib/tunez/music.ex
@@ -0,0 +1,14 @@
+defmodule Tunez.Music do
+  use Ash.Domain,
+    otp_app: :tunez
+
+  resources do
+    resource Tunez.Music.Artist do
+      define :create_artist, action: :create
+      define :get_artists, action: :read
+      define :get_artist_by_id, action: :read, get_by: :id
+      define :update_artist, action: :update
+      define :delete_artist, action: :destroy
+    end
+  end
+end
